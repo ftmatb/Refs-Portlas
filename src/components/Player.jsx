70a1b1a399962d0123d67c256bc27f2de6310423
Sofi9371 <146138151+ftmatb@users.noreply.github.com>
2025-08-10 15:44:45 +0200

    fragments-connnecting and accessing html via refs-manipulating DOM via refs

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,30 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const playerName = useRef();
+
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+
+  // function handleClick() {
+  //   setEnteredPlayerName(playerName.current.value);
+  //   playerName.current.value = ""; // Clear the input field after setting the name
+  // }
+
+  function handleClick() {
+    const name = playerName.current.value.trim(); // trim spaces
+    if (name === "") {
+      setEnteredPlayerName(null); // reset to fallback
+    } else {
+      setEnteredPlayerName(name);
+    }
+    playerName.current.value = ""; // clear input
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredPlayerName ?? "unknown player"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
