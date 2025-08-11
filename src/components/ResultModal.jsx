f4ab21771cd18c2d69aaa1f2579b8e23769b6d40
Sofi9371 <146138151+ftmatb@users.noreply.github.com>
2025-08-11 21:17:20 +0200

    forwarding refs to custom components

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,6 +1,8 @@
-export default function ResultModal({ result, targetTime }) {
+import { forwardRef } from 'react';
+
+const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
   return (
-    <dialog className="result-modal" open> 
+    <dialog ref={ref} className="result-modal">
       <h2>You {result}</h2>
       <p>
         The targey time was <strong>{targetTime}</strong> seconds.
@@ -13,4 +15,5 @@ export default function ResultModal({ result, targetTime }) {
       </form>
     </dialog>
   );
-}
+});
+ export default ResultModal;
